bc9bd5d3930f31f6a3cc85cab7a3854f59a5dc51
Randy Faison <randyfaison@randys-mbp.myfiosgateway.com>
2021-05-11 09:39:37 -0400

    questions array updated, software installed

---
+++ utils/questions.js
@@ -0,0 +1,49 @@
+//array of questions
+
+exports.questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "please enter project name"
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "please enter a brief description"
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: "please enter installation instructions"
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "please enter usage information"
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: "please enter contribution guidelines"
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: "please enter test intructions"
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: "please choose a license"
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: "please enter GitHib username"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "please enter email address"
+    }
+];
