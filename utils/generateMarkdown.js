125bf1b301e3ca851fc96df04b49b5061c1b5d9e
Randy Faison <randyfaison@randys-mbp.myfiosgateway.com>
2021-05-11 09:46:22 -0400

    working with license badges

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,8 @@
+const renderLicenseBadge = (license) => {
+    let licenseMarkdownText;
+    
+    switch (license){
+        case 'JSON License':
+            licenseMarkdownText = '[![License: JSON](https://img.shields.io/badge/License-JSON-red.svg)](https://spdx.org/licenses/JSON.html)';
+    }
+    }
