a125ef4bf636425ebd135d15c69f549728a05bab
Randy Faison <randyfaison@randys-mbp.myfiosgateway.com>
2021-05-11 09:51:05 -0400

    index.js file added

---
+++ index.js
@@ -0,0 +1,6 @@
+//include needed packages
+const fs = require('fs');
+const inquirer = require ('inquirer');
+//modules
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const questions = require('./utils/questions.js');
