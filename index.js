63743f67ec89a1023cf77d03e55b57f75f0854c5
Randy Faison <randyfaison@randys-mbp.myfiosgateway.com>
2021-05-12 09:11:09 -0400

    index write to file

--- index.js
+++ index.js
@@ -2,5 +2,26 @@
 const fs = require('fs');
 const inquirer = require ('inquirer');
 //modules
-const generateMarkdown = require('./utils/generateMarkdown.js');
 const questions = require('./utils/questions.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+//write readme file
+const writetoFile = (inputuser) => {
+fs.writeFile('README.md', inputuser, (error) =>
+error ? console.log('error') : console.log('success'));
+}
+
+const userPrompt = () => {
+    return inquirer.prompt(questions.questions)
+}
+
+
+const init = () => {
+    userPrompt()
+        .then ((inputuser) => {
+
+            const markdown = generateMarkdown.generateMarkdown(inputuser)
+
+            writetoFile(markdown)
+        })
+}
