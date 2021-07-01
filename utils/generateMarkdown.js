2b8bc77ee8de65103edeb49e21589b4acd073073
Randy Faison <randyfaison@gmail.com>
2021-07-01 13:20:59 -0400

    renderLicense badge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,5 +4,21 @@ const renderLicenseBadge = (license) => {
     switch (license){
         case 'JSON License':
             licenseMarkdownText = '[![License: JSON](https://img.shields.io/badge/License-JSON-red.svg)](https://spdx.org/licenses/JSON.html)';
+            break;
+
+        case 'MIT License':
+        licenseMarkdownText = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+        break;
     }
+    return licenseMarkdownText
+
+    }
+
+    const renderLicenseLink = (license) => {
+
+        let licenseLinkURL;
+
+        switch (license) {
+            case 
+        }
     }
