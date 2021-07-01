37b7aee4f3382ab23900605356cb7c46cd86b0f3
Randy Faison <randyfaison@gmail.com>
2021-07-01 14:32:03 -0400

    fixed formatting

--- README.md
+++ README.md
@@ -1,28 +1,28 @@
 
-        # Project Name:
-        ReadMe Generator
-        ## License
-        undefined : [View License](undefined)
-        ## Table of Contents
-        - [Title](#Project-Name)
-        - [License](#License)
-        - [Description](#Description)
-        - [Installation](#Installation)
-        - [Usage](#Usage)
-        - [Contributions](#Contributions)
-        - [Tests](#Tests)
-        - [Questions](#Questions)
-        ## Description
-        When creating an open source project on GitHub, it’s important to have a high-quality README for the app. This should include what the app is for, how to use the app, how to install it, how to report issues, and how to make contributions&mdash;this last part increases the likelihood that other developers will contribute to the success of the project. 
-        ## Installation
-        Navigate to the root directory of this repository and run the following commands to install necessary dependencies:
-          npm install
-        ## Usage
-        
-        ## Contributions 
-        undefined
-        ## Tests 
-        undefined
-        ## Questions 
-        For issues, questions, and comments please contact test@test.com or visit [https://github.com/undefined](https://github.com/undefined) 
+# Project Name:
+t
+## License
+undefined : [View License](undefined)
+## Table of Contents
+    - [Title](#Project-Name)
+    - [License](#License)
+    - [Description](#Description)
+    - [Installation](#Installation)
+    - [Usage](#Usage)
+    - [Contributions](#Contributions)
+    - [Tests](#Tests)
+    - [Questions](#Questions)
+## Description
+t
+## Installation
+Navigate to the root directory of this repository and run the following commands to install necessary dependencies:
+t
+## Usage
+t
+## Contributions 
+undefined
+## Tests 
+undefined
+## Questions 
+For issues, questions, and comments please contact t or visit [https://github.com/undefined](https://github.com/undefined) 
         
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,73 +1,73 @@
 const renderLicenseBadge = (license) => {
     let licenseMarkdownText;
-    
-    switch (license){
+
+    switch (license) {
         case 'JSON License':
             licenseMarkdownText = '[![License: JSON](https://img.shields.io/badge/License-JSON-red.svg)](https://spdx.org/licenses/JSON.html)';
             break;
 
         case 'MIT License':
-        licenseMarkdownText = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
-        break;
+            licenseMarkdownText = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+            break;
     }
     return licenseMarkdownText
 
+}
+
+const renderLicenseLink = (license) => {
+
+    let licenseLinkURL;
+
+    switch (license) {
+        case 'JSON License':
+            licenseLinkURL = 'https://spdx.org/licenses/JSON.html';
+            break;
+
+        case 'MIT License':
+            licenseLinkURL = 'https://opensource.org/licenses/MIT';
     }
+    return licenseLinkURL;
+}
 
-    const renderLicenseLink = (license) => {
+const generateMarkdown = (data) => {
 
-        let licenseLinkURL;
+    const { name, license, description, installation, usage, contributions, tests, email, questions } = data;
 
-        switch (license) {
-            case 'JSON License': 
-                licenseLinkURL = 'https://spdx.org/licenses/JSON.html';
-                break;
+    const licenseBadge = renderLicenseBadge(license);
+    const licenseLinkURL = renderLicenseLink(license);
 
-            case 'MIT License':
-                licenseLinkURL = 'https://opensource.org/licenses/MIT';
-        }
-        return licenseLinkURL;
-    }
-
-    const generateMarkdown = (data) =>  {
-        
-        const { name, license, description, installation, usage, contributions, tests, email, questions } = data;
-      
-        const licenseBadge = renderLicenseBadge(license);
-        const licenseLinkURL = renderLicenseLink(license);
-        
-        return `
-        # Project Name:
-        ${name}
-        ## License
-        ${licenseBadge} : [View License](${licenseLinkURL})
-        ## Table of Contents
-        - [Title](#Project-Name)
-        - [License](#License)
-        - [Description](#Description)
-        - [Installation](#Installation)
-        - [Usage](#Usage)
-        - [Contributions](#Contributions)
-        - [Tests](#Tests)
-        - [Questions](#Questions)
-        ## Description
-        ${description}
-        ## Installation
-        Navigate to the root directory of this repository and run the following commands to install necessary dependencies:
-          ${installation}
-        ## Usage
-        ${usage}
-        ## Contributions 
-        ${contributions}
-        ## Tests 
-        ${tests}
-        ## Questions 
-        For issues, questions, and comments please contact ${email} or visit [https://github.com/${questions}](https://github.com/${questions}) 
+    return `
+# Project Name:
+${name}
+## License
+${licenseBadge} : [View License](${licenseLinkURL})
+## Table of Contents
+    - [Title](#Project-Name)
+    - [License](#License)
+    - [Description](#Description)
+    - [Installation](#Installation)
+    - [Usage](#Usage)
+    - [Contributions](#Contributions)
+    - [Tests](#Tests)
+    - [Questions](#Questions)
+## Description
+${description}
+## Installation
+Navigate to the root directory of this repository and run the following commands to install necessary dependencies:
+${installation}
+## Usage
+${usage}
+## Contributions 
+${contributions}
+## Tests 
+${tests}
+## Questions 
+For issues, questions, and comments please contact ${email} or visit [https://github.com/${questions}](https://github.com/${questions}) 
         `
-      
-      }
-      
-      //export generateMarkdown function to use in other modules
-      module.exports = {
-        generateMarkdown: generateMarkdown,
-      }
+
+}
+
+//export generateMarkdown function to use in other modules
+module.exports = {
+    generateMarkdown: generateMarkdown,
+}
